0ce83c557de667bc2213a40a31c0f2b5668b8534
Felipe Nolleto Nascimento <felipenolletonascimento@gmail.com>
2015-10-22 21:51:49 -0200

    Firt commit

---
+++ SessionHelper/Models/SessionProperty.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionHelper.Models
+{
+    public class SessionProperty<T>
+    {
+        private string key { get; set; }
+        private Type type { get { return typeof(T); } }
+
+        public SessionProperty(string key)
+        {
+            this.key = key;
+        }
+
+        public SessionProperty<T> Set(T value)
+        {
+            Session.SetSessionProperty(key, value);
+            return this;
+        }
+
+        public T Get()
+        {
+            return Session.GetSessionProperty<T>(key);
+        }
+
+        public T GetOrDefault()
+        {
+            return GetValue((current) =>
+            {
+                if (!type.IsPrimitive && type != typeof(string)) return Activator.CreateInstance<T>();
+                return current;
+            });
+        }
+
+        public T GetOrDefault(T defaultValue)
+        {
+            return GetValue((current) =>
+            {
+                return defaultValue;
+            });
+        }
+
+        public SessionProperty<T> Reset()
+        {
+            Set(default(T));
+            return this;
+        }
+
+        private T GetValue(Func<T, T> valueNull = null)
+        {
+            T currentValue = Get();
+            if (currentValue == null && valueNull != null)
+            {
+                currentValue = valueNull(currentValue);
+            }
+            return currentValue;
+        }
+    }
+}
---
+++ SessionHelper/Session.cs
@@ -0,0 +1,63 @@
+﻿using SessionHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.SessionState;
+
+namespace SessionHelper
+{
+    public static class Session
+    {
+        internal static HttpSessionState session
+        {
+            get
+            {
+                return HttpContext.Current.Session;
+            }
+        }
+
+        internal static TModel GetSessionProperty<TModel>(string prop)
+        {
+            if (Session.session == null)
+                return default(TModel);
+
+            var obj = Session.session[prop];
+            if (obj != null)
+            {
+                return (TModel)obj;
+            }
+            return default(TModel);
+        }
+
+        internal static void SetSessionProperty(string prop, object value)
+        {
+            if (Session.session != null)
+            {
+                Session.session[prop] = value;
+            }
+        }
+
+        public static SessionProperty<TModel> CreateProperty<TModel>(string name)
+        {
+            return new SessionProperty<TModel>(name);
+        }
+
+        public static SessionProperty<object> CreateProperty(string name)
+        {
+            return new SessionProperty<object>(name);
+        }
+
+        public static TModel GetValue<TModel>(string name)
+        {
+            return new SessionProperty<TModel>(name).Get();
+        }
+
+        public static object GetValue(string name)
+        {
+            return new SessionProperty<object>(name).Get();
+        }
+    }
+}
